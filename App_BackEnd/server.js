d354222326080e05ee0cba055631975e1c83b449
Javier Solares Servent <javiss1254@hotmail.com>
2018-11-11 12:21:34 -0600

    Se agregó una conexión al archivo de rutas en el servidor, así como también una instancia de Body Parser.

--- App_BackEnd/server.js
+++ App_BackEnd/server.js
@@ -5,9 +5,15 @@ const cors = require('cors');
 
 const app = express();
 
+const router = require('./rutas/api');
+
 mongoose.connect('mongodb://localhost/pokedex');
 
 app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use('/api/v1/pokemon', router);
 
 app.listen(5000, function() {
     console.log('Node server is running..');
