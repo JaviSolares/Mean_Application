7fcec18b4967f0125c5013c2ac3e789df2bd90a6
Javier Solares Servent <javiss1254@hotmail.com>
2018-11-13 14:18:09 -0600

    Se completaron los 4 métodos del CRUD del back-end, y se comprobó el funcionamiento de los mismos. Se da por concluida la construcción del back-end.

--- App_BackEnd/rutas/api.js
+++ App_BackEnd/rutas/api.js
@@ -1,24 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const Monster = require('../modelos/pokemon');
 
 router.get('/', function(req, res, next) {
-    res.send('get route tested.');
+    Monster.find({}).then(function(pokemones) {
+        res.send(pokemones);
+    });
 });
 
 router.get('/:id', function(req, res, next) {
-
+    Monster.findOne({ id: req.params.id })
+        .then(function(pokemon) {
+            res.send(pokemon);
+        });
 });
 
 router.post('/add-new', function(req, res, next) {
-
+    Monster.create(req.body).then(function(pokemon) {
+        res.send(pokemon);
+    }).catch(next);
 });
 
 router.put('/update-data/:id', function(req, res, next) {
-
+    Monster.findOneAndUpdate({ id: req.params.id }, req.body)
+        .then(function() {
+            Monster.findOne({ id: req.params.id })
+                .then(function(pokemon) {
+                    res.send(pokemon);
+                });
+        });
 });
 
 router.delete('/delete-data/:id', function(req, res, next) {
-
+    Monster.findOneAndDelete({ id: req.params.id })
+        .then(function(pokemon) {
+            res.send(pokemon);
+        });
 });
 
 module.exports = router;
