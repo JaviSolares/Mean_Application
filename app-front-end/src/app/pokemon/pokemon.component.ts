efc5587937e8f925982eac8c7b642dfa550dac32
Javier Solares Servent <javiss1254@hotmail.com>
2018-11-16 15:52:30 -0600

    Se inyectó el Pokemon Service en el Pokemon Component. Al método "getPokemones" se le cambió el nombre a "getMonsters".

--- app-front-end/src/app/pokemon/pokemon.component.ts
+++ app-front-end/src/app/pokemon/pokemon.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-pokemon',
   templateUrl: './pokemon.component.html',
-  styleUrls: ['./pokemon.component.css']
+  styleUrls: ['./pokemon.component.css'],
+  //providers: [PokemonService]
 })
 export class PokemonComponent implements OnInit {
 
   pokemones: Pokemon[] = [];
 
-  getPokemones() {
-    
+  constructor(private pokemonService: PokemonService) { }
+
+  getMonsters() {
+    this.pokemonService.getPokemones()
+      .subscribe(monsters => {
+        this.pokemones = monsters;
+        console.log('Data from pokemon service: ' + this.pokemones[0].nombre);
+      });
   }
 
-  constructor() { }
-
   ngOnInit() {
+    this.getMonsters();
   }
 
 }
