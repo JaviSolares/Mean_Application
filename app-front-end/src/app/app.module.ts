e5750a4848a22dd8289e8a51f4b6f45e55f3304b
Javier Solares Servent <javiss1254@hotmail.com>
2018-11-16 12:34:41 -0600

    Se importaron los módulos Http y Forms al módulo App Module.

--- app-front-end/src/app/app.module.ts
+++ app-front-end/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
@@ -10,7 +12,9 @@ import { PokemonComponent } from './pokemon/pokemon.component';
     PokemonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
