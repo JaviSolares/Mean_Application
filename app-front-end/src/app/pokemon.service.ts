977fa8c323a09cf7c4d5606e25675c91c966f75b
Javier Solares Servent <javiss1254@hotmail.com>
2018-11-16 13:48:00 -0600

    Se implementaron métodos Get y Post en el Pokemon Service.

--- app-front-end/src/app/pokemon.service.ts
+++ app-front-end/src/app/pokemon.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
+  
+  constructor(private http: Http) { }
 
-  constructor() { }
+  getPokemones() {
+    return this.http.get('http://localhost5000:/api/v1/pokemon')
+      .pipe(
+        map(res => res.json())
+      );
+  }
+
+  getPokemon(id) {
+    const url = `http://localhost5000:/api/v1/pokemon/${id}`;
+    return this.http.get(url)
+      .pipe(
+        map(res => res.json())
+      );
+  }
+
+  addPokemon(newPokemon) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost5000:/api/v1/pokemon', newPokemon, {headers: headers})
+      .pipe(
+        map(res => res.json())
+      );
+  }
+
 }
