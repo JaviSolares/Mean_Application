33721e3aad15c600940e9e176b71b4ac7f4c0c81
Javier Solares Servent <javiss1254@hotmail.com>
2018-11-18 15:29:37 -0600

    Se implementó un servicio de Redis para almacenar en caché la información de los Get efectuados al momento de hacer consultas en la lista de pokemon.

--- App_BackEnd/rutas/api.js
+++ App_BackEnd/rutas/api.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const Monster = require('../modelos/pokemon');
+const redis = require('redis');
+
+var client = redis.createClient(6379, 'localhost');
+
+client.on('connect', function() {
+    console.log('Connected to Redis..');
+});
+
+client.on('error', function() {
+    console.log('Some error in connecting the redis cache');
+});
 
 router.get('/', function(req, res, next) {
     Monster.find({}).then(function(pokemones) {
@@ -9,10 +20,28 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:id', function(req, res, next) {
-    Monster.findOne({ id: req.params.id })
-        .then(function(pokemon) {
-            res.send(pokemon);
-        });
+    client.get(req.params.id, function(err, reply) {
+        if (err) {
+            next(null);
+        } 
+        /*else if (reply) {
+            next(JSON.parse(reply));
+        }*/
+        else {
+            Monster.findOne({ id: req.params.id }, 
+                function(err, doc) {
+                    if (err || !doc) {
+                        next(null);
+                    }
+                    else {
+                        client.set(req.params.id, JSON.stringify(doc));
+                    }
+                })
+                .then(function(pokemon) {
+                    res.send(pokemon);
+                });
+        }
+    });
 });
 
 router.post('/', function(req, res, next) {
@@ -37,5 +66,30 @@ router.delete('/:id', function(req, res, next) {
             res.send(pokemon);
         });
 });
+
+getPokemonCached = function(collection, redis, id, callback) {
+    redis.get(id, function(err, reply) {
+        if (err) {
+            callback(null);
+        } 
+        else if (reply) {
+            callback(JSON.parse(reply));
+        }
+        else {
+            collection.findOne({ id: id }, 
+                function(err, doc) {
+                    if (err || !doc) {
+                        callback(null);
+                    }
+                    else {
+                        redis.set(id, JSON.stringify(doc),
+                        function() {
+                            callback(doc);
+                        });
+                    }
+                });
+        }
+    });
+};
 
 module.exports = router;
--- App_BackEnd/server.js
+++ App_BackEnd/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const redis = require('redis');
 
 const app = express();
 
