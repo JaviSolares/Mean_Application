7dac111351c2d521cdd9aa21389dec341ef9f43a
JaviSolares <javiss1254@hotmail.com>
2018-11-23 17:41:53 -0600

    Se importò el componente Dashboard al mòdulo AppRouting.

--- app-front-end/src/app/app-routing.module.ts
+++ app-front-end/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PokemonComponent } from './pokemon/pokemon.component';
 import { InfoPokemonComponent } from './info-pokemon/info-pokemon.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const rutas: Routes = [
-  { path: '', redirectTo: '/pokemones', pathMatch: 'full' },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: InfoPokemonComponent },
   { path: 'pokemones', component: PokemonComponent }
 ];
