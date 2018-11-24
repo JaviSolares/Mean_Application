b39267ab349ab1a78a2fbca1e0cae1ca4c9f07f5
JaviSolares <javiss1254@hotmail.com>
2018-11-24 13:15:27 -0600

    Se agregò la IPv4 de la nube a la aplicaciòn.

--- app-front-end/src/app/app.constants.ts
+++ app-front-end/src/app/app.constants.ts
@@ -1,3 +1,3 @@
 export class AppConstants {
-    public static get baseURL(): string { return 'http://localhost:5000/api/v1/pokemon'; }
+    public static get baseURL(): string { return 'http://ec2-18-224-150-122.us-east-2.compute.amazonaws.com:5000/api/v1/pokemon'; }
 }
